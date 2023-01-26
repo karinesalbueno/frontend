ff4f234b0347c718ed01b37fcb7fb826f4c9faa4
Karine <karine.costa@somagrupo.com.br>
2023-01-26 13:16:18 -0300

    usando redux

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import Header from 'src/components/header'
+import Button from 'src/components/button'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Button />
+    </>
+  )
+}
