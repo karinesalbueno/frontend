022b5793de7ca59c484d6b9b4a5b27906b7cde0b
Karine <karine.costa@somagrupo.com.br>
2023-01-26 21:49:47 -0300

    [styles]: botão comprar

--- src/components/productItem.tsx
+++ src/components/productItem.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import logoBag from "../assets/shopping-bag.svg"
+
 import * as Styled from '../styles/Home'
 
 interface ICardProps {
@@ -13,18 +16,20 @@ export default function ProductItem(props: ICardProps) {
     const { title, price, description, image, id } = props;
     return (
         <Styled.CardItemLi>
-            <img src={image} alt="image" />
+            <div className='content'>
+                <img src={image} alt="image" />
+                <Styled.InformationsDiv>
+                    <h3>{title}</h3>
+                    <div className='price'>R${price}</div>
+                </Styled.InformationsDiv>
 
-            <Styled.InformationsDiv>
-                <h3>{title}</h3>
-                <div className='price'>R${price}</div>
-            </Styled.InformationsDiv>
-
-            <small>{description}</small>
-
-            <div>
-                <button onClick={() => { }}>Comprar</button>
+                <small>{description}</small>
             </div>
+
+            <Styled.Button onClick={() => { }}>
+                <Image src={logoBag} alt="bag" />
+                <span>comprar</span>
+            </Styled.Button>
         </Styled.CardItemLi>
     )
 }
--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -15,15 +15,22 @@ export const CardItemUl = styled.ul`
     grid-template-columns: repeat(4, 1fr);
     gap: 40px 18px;
     border-radius: 8px;
+    padding: 0;
 `;
 
 export const CardItemLi = styled.li`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     background: #ffffff;
     box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.135216);
     border-radius: 8px;
-    padding: 10px 20px;
 
     list-style: none;
+
+    .content {
+        padding: 10px 15px;
+    }
 
     img {
         height: 100px;
@@ -59,5 +66,31 @@ export const InformationsDiv = styled.div`
         color: #ffffff;
         font-weight: 700;
         font-size: 15px;
+    }
+`;
+
+export const Button = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    background: #0f52ba;
+    height: 32px;
+    cursor: pointer;
+    border: none;
+    border-radius: 0px 0px 8px 8px;
+
+    img {
+        height: 14px;
+    }
+
+    span {
+        text-transform: uppercase;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 18px;
+        color: #ffffff;
+        margin-left: 17px;
     }
 `;
